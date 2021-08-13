2335946edb382deb80d9b87314aa30493d240117
fawda123 <mbafs2012@gmail.com>
2021-08-13 16:41:32 -0400

    wq map figure added

--- R/figures.R
+++ R/figures.R
@@ -16,6 +16,8 @@ library(USAboundaries)
 data(rsallpts)
 data(ppseg)
 data(segmask)
+data(rswqdat)
+data(rsstatloc)
 
 # map ---------------------------------------------------------------------
 
@@ -182,4 +184,132 @@ pout <- p1 + p2
 
 jpeg(here('figs/map.jpeg'), height = 4.2, width = 9, family = 'serif', units = 'in', res = 500)
 print(pout)
+dev.off()
+
+# nutrients, chloropyll map -----------------------------------------------
+
+# nonbay stations
+nonbay <- c('BH01', 'P Port 2', 'P Port 3', 'PM Out', '20120409-01', 'PPC41', 'P Port 4', 'PMB01', 'NGS-S Pond')
+
+# colors 
+vrscols <- rev(RColorBrewer::brewer.pal(n = 9, name = 'RdYlBu'))
+vrscols[7:9] <- 'red'
+
+# ggplot base size
+bssz <- 13
+
+thm <-  theme(
+    axis.title = element_blank(),
+    legend.position = 'bottom', 
+    legend.box= 'vertical'
+  ) 
+
+# combine water quality data with locations
+wqdat <- rswqdat %>% 
+  filter(var %in% c('tn', 'chla', 'secchi')) %>% 
+  filter(!station %in% nonbay) %>% 
+  inner_join(rsstatloc, ., by = 'station') %>% 
+  mutate(
+    lng = st_coordinates(.)[, 1], 
+    lat = st_coordinates(.)[, 2],
+    dategrp = floor_date(date, unit = 'week'), 
+    inrng = case_when(
+      inrng %in% c('below', 'in range') & var %in% c('chla', 'tn') ~ 'in range', 
+      inrng %in% c('above') & var %in% c('chla', 'tn') ~ 'above', 
+      inrng %in% c('above', 'in range') & var %in% c('secchi') ~ 'in range',
+      inrng %in% c('below') & var %in% c('secchi') ~ 'below'
+    )
+  ) %>% 
+  select(station, date, dategrp, var, val, lat, lng, inrng)
+
+# reference data for ggsn, MUST have geometry named column
+dat_ext <- wqdat %>% 
+  st_bbox %>% 
+  st_as_sfc %>% 
+  st_buffer(dist = set_units(0.01, degree)) %>%
+  st_bbox %>% 
+  unname
+
+# stamen base map
+bsmap1 <- get_stamenmap(bbox = dat_ext, maptype = 'toner-background', zoom = 11)
+
+# change opacity of basemap
+mapatt <- attributes(bsmap1)
+bsmap1_transparent <- matrix(adjustcolor(bsmap1, 
+                                         alpha.f = 0.1), 
+                             nrow = nrow(bsmap1))
+attributes(bsmap1_transparent) <- mapatt
+
+# base map
+bsmap <- ggmap(bsmap1_transparent)
+
+toplo1 <- wqdat %>% 
+  filter(var == 'tn')
+
+# static plot
+p1 <- bsmap +
+  geom_point(data = toplo1, aes(x = lng, y = lat, size = val, fill = val, group = dategrp, color = inrng), pch = 21, alpha = 0.8) +
+  scale_fill_gradientn('mg/L', trans = 'log10', colours = vrscols) +
+  scale_color_manual('In normal range?', values = c('black', 'lightgrey')) +
+  scale_size('mg/L', range = c(0.5, 5), trans = 'log10') + 
+  coord_map() + 
+  guides(
+    fill = guide_legend(order = 1), 
+    size = guide_legend(order = 1), 
+    color = guide_legend(override.aes = list(size = 6), order = 2)
+  ) + 
+  theme_bw(base_size = bssz) +
+  thm + 
+  labs(
+    title = '(a) Total Nitrogen'
+  )
+
+toplo2 <- wqdat %>% 
+  filter(var %in% 'chla') %>% 
+  mutate(val = pmin(30, val))
+
+brks <- c(1, 3, 10 , 30)
+lbs <- c('1', '3', '10', '>30')
+p2 <- bsmap +
+  geom_point(data = toplo2, aes(x = lng, y = lat, size = val, fill = val, group = dategrp, color = inrng), pch = 21, alpha = 0.8) +
+  scale_fill_gradientn('ug/L', trans = 'log10', breaks = brks, labels = lbs, colours = vrscols) +
+  scale_color_manual('In normal range?', values = c('black', 'lightgrey'), guide = T) +
+  scale_size('ug/L', range = c(0.5, 5), breaks = brks, labels = lbs, trans = 'log10') + 
+  coord_map() + 
+  guides(
+    fill = guide_legend(order = 1), 
+    size = guide_legend(order = 1), 
+    color = guide_legend(override.aes = list(size = 6), order = 2)
+  ) + 
+  theme_bw(base_size = bssz) +
+  thm + 
+  labs(
+    title = '(b) Chlorophyll-a'
+  )
+
+toplo3 <- wqdat %>% 
+  filter(var %in% 'secchi')
+
+brks <- c(1, 2, 3, 4)
+p3 <- bsmap +
+  geom_point(data = toplo3, aes(x = lng, y = lat, size = val, fill = val, group = dategrp, color = inrng), pch = 21, alpha = 0.8) +
+  scale_fill_gradientn('meters',colours = rev(vrscols), breaks = brks) +
+  scale_color_manual('In normal range?', values = c('black', 'grey'), guide = T) +
+  scale_size('meters', range = c(6, 0.5), breaks = brks) + 
+  coord_map() + 
+  guides(
+    fill = guide_legend(order = 1), 
+    size = guide_legend(order = 1), 
+    color = guide_legend(override.aes = list(size = 6), order = 2)
+  ) + 
+  theme_bw(base_size = bssz) +
+  thm + 
+  labs(
+    title = '(c) Secchi'
+  )
+
+p <- p1 + p2 + p3
+
+jpeg(here('figs/wqmap.jpeg'), height = 7, width = 12, units = 'in', res = 500, family = 'serif')
+print(p)
 dev.off()
