3a767864e103f668437b257728063f0c73afe352
fawda123 <mbafs2012@gmail.com>
2021-10-12 16:31:54 -0400

    mini script for long-term baseline and response data for chl, tn, secchi, by area aggregated or area stations

--- R/wqbyarea.R
+++ R/wqbyarea.R
@@ -29,23 +29,7 @@ vr <- c('chla', 'tn', 'secchi')
 cols <- c("#E16A86", "#50A315", "#009ADE")
 names(cols) <- levels(ppseg$area)
 
-datin <- rswqdat %>% 
-  filter(date < as.Date('2021-08-01')) %>% 
-  filter(var %in% c('tn', 'chla', 'secchi')) %>% 
-  filter(!qual %in% c('S', 'U')) 
-# unique weeks
-wklv <- rswqdat %>%
-  pull(date) %>% 
-  range
-
-wklv <- seq.Date(wklv[1], wklv[2],by = 'days') %>% 
-  floor_date(unit = 'week') %>% 
-  unique %>% 
-  crossing(date = ., area = c('Area 1', 'Area 2', 'Area 3')) %>% 
-  mutate(
-    fillcl = factor(area, levels = unique(area), labels = cols), 
-    fillcl = as.character(fillcl)
-  )
+# aggregated across stations ----------------------------------------------
 
 # monitoring data
 rswqtmp <- rswqdat %>% 
@@ -102,3 +86,46 @@ write.csv(wqbyarea, file = '~/Desktop/wqbyarea.csv', row.names = F)
 ggplot(wqbyarea, aes(x = date, y = avev)) +
   geom_line() + 
   facet_wrap(var ~ area, scales = 'free_y')
+
+# not aggregated across stations ------------------------------------------
+
+# monitoring data
+rswqtmp <- rswqdat %>% 
+  filter(var %in% vr) %>% 
+  filter(!station %in% nonbay) %>% 
+  inner_join(rsstatloc, ., by = c('station', 'source')) %>% 
+  st_intersection(ppsegbf) %>% 
+  st_set_geometry(NULL) %>% 
+  select(-bswqstation, -nrmrng, -source, -source_lng, -uni, -lbunis) %>% 
+  mutate(
+    date = floor_date(date, unit = 'week'),
+    cens = grepl('U', qual)
+  ) %>% 
+  left_join(parms, by = 'var') %>% 
+  select(station, date, var, val, area, uni)
+
+# baseline data
+bswqtmp <- bswqdat %>% 
+  select(-source, -uni) %>% 
+  filter(var %in% vr) %>%
+  filter(!(var == 'secchi' & grepl('S', qual))) %>% # remove secchi on bottom
+  filter(yr > 2005) %>% 
+  filter(!is.na(val)) %>% 
+  inner_join(bsstatloc, ., by = 'station') %>% 
+  st_intersection(ppsegbf) %>% 
+  st_set_geometry(NULL) %>% 
+  mutate(
+    date = floor_date(date, unit = 'month'),
+    cens = grepl('U', qual)) %>% 
+  left_join(parms, by = 'var') %>% 
+  select(station, date, var, val, area, uni)
+
+wqbyarea <- bind_rows(bswqtmp, rswqtmp) %>% 
+  arrange(date, var, area)
+
+write.csv(wqbyarea, file = '~/Desktop/wqbyarea.csv', row.names = F)
+
+ggplot(wqbyarea, aes(x = date, y = avev)) +
+  geom_line() + 
+  facet_wrap(var ~ area, scales = 'free_y')
+
