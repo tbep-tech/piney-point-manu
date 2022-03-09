41171ec8196bfa72fc2e1234ebcc6bad026eb3a8
fawda123 <mbafs2012@gmail.com>
2022-03-08 20:15:52 -0500

    starting second revision

--- R/figures.R
+++ R/figures.R
@@ -266,7 +266,7 @@ jpeg(here('figs/map.jpeg'), height = 4.2, width = 9, family = 'serif', units = '
 print(pout)
 dev.off()
 
-jpeg(here('revision/Fig1.jpeg'), height = 4.2, width = 9, family = 'serif', units = 'in', res = 500)
+jpeg(here('revision2/Fig1.jpeg'), height = 4.2, width = 9, family = 'serif', units = 'in', res = 500)
 print(pout)
 dev.off()
 
@@ -407,7 +407,7 @@ jpeg(here('figs/wqmap.jpeg'), height = 7, width = 12, units = 'in', res = 500, f
 print(p)
 dev.off()
 
-jpeg(here('revision/Fig2.jpeg'), height = 7, width = 12, units = 'in', res = 500, family = 'serif')
+jpeg(here('revision2/Fig2.jpeg'), height = 7, width = 12, units = 'in', res = 500, family = 'serif')
 print(p)
 dev.off()
 
@@ -441,7 +441,7 @@ jpeg(here('figs/wqgam.jpeg'), height = 6, width = 8.5, units = 'in', res = 500, 
 print(p)
 dev.off()
 
-jpeg(here('revision/Fig3.jpeg'), height = 6, width = 8.5, units = 'in', res = 500, family = 'serif')
+jpeg(here('revision2/Fig3.jpeg'), height = 6, width = 8.5, units = 'in', res = 500, family = 'serif')
 print(p)
 dev.off()
 
@@ -480,7 +480,7 @@ jpeg(here('figs/trnex.jpeg'), height = 7, width = 8, units = 'in', res = 500, fa
 print(p)
 dev.off()
 
-jpeg(here('revision/Fig4.jpeg'), height = 7, width = 8, units = 'in', res = 500, family = 'serif')
+jpeg(here('revision2/Fig4.jpeg'), height = 7, width = 8, units = 'in', res = 500, family = 'serif')
 print(p)
 dev.off()
 
@@ -612,7 +612,7 @@ jpeg(here('figs/trnfrq.jpeg'), height = 6, width = 9, units = 'in', res = 500, f
 print(p)
 dev.off()
 
-jpeg(here('revision/Fig5.jpeg'), height = 6, width = 9, units = 'in', res = 500, family = 'serif')
+jpeg(here('revision2/Fig5.jpeg'), height = 6, width = 9, units = 'in', res = 500, family = 'serif')
 print(p)
 dev.off()
 
@@ -965,13 +965,13 @@ jpeg(here('figs/redtide.jpeg'), height = 11, width = 9, units = 'in', res = 500,
 print(p)
 dev.off()
 
-jpeg(here('revision/Fig6.jpeg'), height = 11, width = 9, units = 'in', res = 500, family = 'serif')
+jpeg(here('revision2/Fig6.jpeg'), height = 11, width = 9, units = 'in', res = 500, family = 'serif')
 print(p)
 dev.off()
 
 # timeline copy -----------------------------------------------------------
 
-file.copy(here('figs/timeline.jpg'), here('revision/Fig7.jpg'))
+file.copy(here('figs/timeline.jpg'), here('revision2/Fig7.jpg'))
 
 # nutrient flow -----------------------------------------------------------
 
@@ -1245,7 +1245,7 @@ jpeg(here('figs/nutrientflow.jpeg'), height = 8, width = 6, units = 'in', res = 
 print(p)
 dev.off()
 
-jpeg(here('revision/Fig8.jpeg'), height = 8, width = 6, units = 'in', res = 500, family = 'serif')
+jpeg(here('revision2/Fig8.jpeg'), height = 8, width = 6, units = 'in', res = 500, family = 'serif')
 print(p)
 dev.off()
 
